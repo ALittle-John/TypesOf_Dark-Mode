8b0d7747290321b3d9aa56789572dee6f2d4430d
ALittle-John <joaopsousaqi@gmail.com>
2023-08-04 16:59:28 -0300

    feat: adding idea folders and black circle reusable

---
+++ Types_of_Dark-Mode/src/components/1CmmBlackCircle/bc.jsx
@@ -0,0 +1,12 @@
+import './bc.css'
+
+function CmmBlackCircle() {
+  return (
+    <>
+      <input type="checkbox" name="BlackCircle" />
+      <label htmlFor="BlackCircle" ></label>
+    </>
+  )
+}
+
+export default CmmBlackCircle
