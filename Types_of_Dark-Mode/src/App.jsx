b099573e27abde073dbd7c27396f9794ff2ff5ed
ALittle-John <joaopsousaqi@gmail.com>
2023-08-04 17:12:50 -0300

    refactor: basic style and component to locate myself

--- Types_of_Dark-Mode/src/App.jsx
+++ Types_of_Dark-Mode/src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
+import CmmBlackCircle from './components/1CmmBlackCircle/bc'
+
 
 function App() {
   
-
-
   return (
-    <>
-      
-    </>
+    <div>
+      <CmmBlackCircle/>
+      <CmmBlackCircle/>
+      <CmmBlackCircle/>
+      <CmmBlackCircle/>
+      <CmmBlackCircle/>
+    </div>
   )
 }
 
