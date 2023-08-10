476738c1b838eaf0f6db89b12bf5e6b670c427d1
ALittle-John <joaopsousaqi@gmail.com>
2023-08-10 20:22:50 -0300

    feat: updating the default function and testing

--- Types_of_Dark-Mode/src/components/Curtain/DMCurtain.jsx
+++ Types_of_Dark-Mode/src/components/Curtain/DMCurtain.jsx
@@ -1,12 +1,21 @@
-import './DMCurtain.css'
-import CmmBlackCircle from "../1CmmBlackCircle/bc"
+import { useState } from 'react';
+
+import './DMCurtain.css';
+import CmmBlackCircle from "../1CmmBlackCircle/bc";
 
 function DMCurtain() {
 
+const [darkMode, setDarkMode] = useState(false)
+
+const handleDarkModeChange = (e) => {
+  setDarkMode(e.target.checked)
+}
+//Associate with the "checked", "useDarkMode" is responsible for actualizating the state.
+
   return (
-    <>
-      <CmmBlackCircle/>
-    </>
+    <div className={darkMode ? 'dark-mode' : ''}>
+      <CmmBlackCircle checked={darkMode} onchange={handleDarkModeChange}/>
+    </div>
   )
 }
 
--- Types_of_Dark-Mode/src/components/Natural/DMNatural.jsx
+++ Types_of_Dark-Mode/src/components/Natural/DMNatural.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 
-import './DMNatural.css'
+import './DMNatural.css';
 import CmmBlackCircle from "../1CmmBlackCircle/bc";
 
 function DMNatural() {
+//The DMNatural (or any other file) is the component that needs to track and manage the dark mode state
+const [darkMode, setDarkMode] = useState(false)
+//If state was created in CmmBlackCircle, it could not be accessed by other components.
+
+const handleDarkModeChange = (e) => {
+  setDarkMode(e.target.checked)
+}
+//Associate with the "checked", "useDarkMode" is responsible for actualizating the state.
   return (
-    <>
-      <CmmBlackCircle/>
-    </>
+    <div className={darkMode ? 'dark-mode' : ''}>
+      <CmmBlackCircle checked={darkMode} onchange={handleDarkModeChange}/>
+    </div>
   )
 }
 
